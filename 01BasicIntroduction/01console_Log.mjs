05c55dbb36c461aa1355b05121383a16eb3f5feb
Anjan K.C. <kcanjan2020@gmail.com>
2023-11-01 23:16:26 +0545

    Variable in JS

---
+++ 01BasicIntroduction/01console_Log.mjs
@@ -0,0 +1,5 @@
+console.log("Hello World !");
+console.log("Hello I am Anjan KC from Himalayan County of the Nepal !");
+ let name="Anjan";
+ let surname="KC";
+ console.log("FullName =>",name,surname);
---
+++ 01BasicIntroduction/02Comments.mjs
@@ -0,0 +1,11 @@
+//Single line Comments
+// This is a single line comment
+console.log("Hello World"); // This is also a single line comment
+
+//=> Multiline Comments
+/*
+This is 
+a multi-line 
+comment.
+*/
+
