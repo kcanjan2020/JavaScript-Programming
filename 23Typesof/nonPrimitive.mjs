15683b7b1d7b0b28f8e33accb6f4335cd6db8511
Anjan K.C. <kcanjan2020@gmail.com>
2023-11-08 21:51:02 +0545

    Typeof Operator in JavaScript

---
+++ 23Typesof/nonPrimitive.mjs
@@ -0,0 +1,18 @@
+//=>Non primitive Data Types: Array,object,Set,null
+
+console.log(typeof null); //object
+
+let arr = [1, 2, 3, 4, 5, 6];
+let type = typeof arr;
+console.log(type); //object
+
+let set = new Set([1, 2, 3, 4, 5]);
+console.log(typeof set); //object
+
+console.log(typeof null); //object
+
+let info = { name: "Anjan", isMarried: true };
+console.log(typeof info); //object
+
+let error = new Error("Error");
+console.log(typeof error); //object
---
+++ 23Typesof/primitive.mjs
@@ -0,0 +1,15 @@
+//==> Primitive Data Type: number,BigInt,string,boolean,undefined.
+let num = 12346;
+console.log(typeof num); //number
+let num2 = 573453537n;
+console.log(typeof num2); // bigint
+let name = "Anjan KC";
+console.log(typeof name); //string
+
+let a = undefined;
+console.log(typeof a); //undefined
+console.log(typeof true); //boolean
+console.log(typeof false); //boolean
+
+//********** null is a non primitive data type */
+console.log(typeof null); // Object
